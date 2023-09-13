a95ac6773e28a09f70ca6da25cb82e59edb131f6
Andreas A. Wold <104062801+andreaswold@users.noreply.github.com>
2023-09-13 12:47:55 +0000

    Implement find_first_treasure_candidate
    Implement function find_first_treasure_candidate and respective test

--- src/treasure_hunt.py
+++ src/treasure_hunt.py
@@ -1,16 +1,24 @@
 from typing import List, Tuple
 
 
-def calculate_slope(coordinate_one: Tuple[float, float], coordinate_two: Tuple[float, float]):
+def calculate_slope(coordinate_one: Tuple[float, float], coordinate_two: Tuple[float, float]) -> float:
     x_1, y_1 = coordinate_one
     x_2, y_2 = coordinate_two
     slope = (y_2 - y_1)/(x_2 - x_1)
     return slope
 
-def calculate_slopes(coordinates: List[Tuple[float, float]]):
+def calculate_slopes(coordinates: List[Tuple[float, float]]) -> List[float]:
     slopes = []
 
     for i in range(len(coordinates) - 1):
         slope: float = calculate_slope(coordinates[i], coordinates[i+1])
         slopes.append(round(slope, 1))
     return slopes
+
+
+def find_first_treasure_candidate(coordinates: List[Tuple[float, float]], slope_threshold: float) -> int:
+    slopes = calculate_slopes(coordinates)
+    for i, slope in enumerate(slopes):
+        if slope >= slope_threshold:
+            return i + 1
+    raise ValueError
--- tests/unit/test_treasure_hunt.py
+++ tests/unit/test_treasure_hunt.py
@@ -1,12 +1,19 @@
 from typing import List
-from treasure_hunt import calculate_slopes
+from treasure_hunt import calculate_slopes, find_first_treasure_candidate
 
 
 def test_correctly_calculated_slopes():
 
-    coordinates =  [(1, 1), (2.0, 1.8), (2.5, 2.1), (3, 2), (4.3, 7.2), (5.3, 6.2)]
+    coordinates = [(1, 1), (2.0, 1.8), (2.5, 2.1), (3, 2), (4.3, 7.2), (5.3, 6.2)]
     expected_slopes = [0.8, 0.6, -0.2, 4.0, -1.0]
 
     slopes: List[float] = calculate_slopes(coordinates)
 
     assert slopes == expected_slopes
+
+def test_find_first_coordinate_with_slope_over_threshold():
+    coordinates = [(1, 1), (2.0, 1.8), (2.5, 2.1), (3, 2), (4.3, 7.2), (5.3, 6.2)]
+    index_with_treasure: int = find_first_treasure_candidate(coordinates, 1)
+
+    expected_index = 4
+    assert index_with_treasure == expected_index
