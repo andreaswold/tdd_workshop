35be4d26413ece642ad0dc2a038529637de4e189
Andreas A. Wold <104062801+andreaswold@users.noreply.github.com>
2023-09-13 13:10:22 +0000

    Move treasure_hunt.py, update cli.py
    Move treasure_hunt to src, update cli to print treasure location

--- src/tdd_workshop/cli.py
+++ src/tdd_workshop/cli.py
@@ -7,6 +7,6 @@ from tdd_workshop.treasure_hunt import find_first_treasure_candidate
 @click.argument("file_name", type=click.Path(exists=True))
 def main(file_name: str) -> None:
     coordinates = read_coordinates(file_name)
-    treasure_index = find_first_treasure_candidate(coordinates, 10)
+    treasure_index = find_first_treasure_candidate(coordinates, 1)
     treasure_location = coordinates[treasure_index]
     print(f"Found the treasure at: {treasure_location}")
